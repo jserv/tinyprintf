f12d8b23089394f293b41a4d42524afbf2156472
Jim Huang <jserv@ccns.ncku.edu.tw>
2024-11-28 12:28:47 +0800

    Eliminate the type conversion warnings
    In many cases, 'char' is treated as a signed type. When a 'char' field
    is defined as a 1-bit wide value, it effectively serves as a single
    sign bit with no data bits, allowing only the values 0 and -1.
    
    This behavior may vary between different compiler vendors and versions.
    However, explicitly declaring the field as unsigned ensures that only
    the values 0 and 1 are valid, eliminating any potential inconsistencies
    across compiler implementations.

--- tinyprintf.c
+++ tinyprintf.c
@@ -75,16 +75,16 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  * Implementation
  */
 struct param {
-    char lz:1;          /**<  Leading zeros */
-    char alt:1;         /**<  alternate form */
-    char uc:1;          /**<  Upper case (for base16 only) */
-    char align_left:1;  /**<  0 == align right (default), 1 == align left */
-    int width;          /**<  field width */
-    int prec;           /**<  precision */
-    char sign;          /**<  The sign to display (if any) */
-    unsigned int base;  /**<  number base (e.g.: 8, 10, 16) */
-    char *bf;           /**<  Buffer to output */
-    size_t bf_len;      /**<  Buffer length */
+    unsigned char lz:1;          /**<  Leading zeros */
+    unsigned char alt:1;         /**<  alternate form */
+    unsigned char uc:1;          /**<  Upper case (for base16 only) */
+    unsigned char align_left:1;  /**<  0 == align right (default), 1 == align left */
+    int width;                   /**<  field width */
+    int prec;                    /**<  precision */
+    char sign;                   /**<  The sign to display (if any) */
+    unsigned int base;           /**<  number base (e.g.: 8, 10, 16) */
+    char *bf;                    /**<  Buffer to output */
+    size_t bf_len;               /**<  Buffer length */
 };
 
 
